bb7d80954aa31c45c89c730e6663394fe2e8988c
TinTin Kalaw <tintin.kalaw@gmail.com>
2016-08-18 12:18:36 +0800

    Attempt at adding segmenter

---
+++ ReaderAffectModelProjects/DataPreprocessor/App/Segmenter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmoEegMerger.App {
+    public class Segmenter {
+        /// <summary>
+        /// TIMESTAMP, AF3, T7, Pz, T8, AF4, SEGMENT, PLEASANTNESS, ATTENTION, SENSITIVITY, APPTITUDE, IS_STRIKING, FROM_EVALUATIVE, FROM_NARRATIVE, FROM_AESTHETIC, FROM_OTHERS
+        /// </summary>
+        private StreamReader mergedLog;
+        public Segmenter(String source) {
+
+        }
+    }
+}
